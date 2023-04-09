6dd2b6f8876dbe28c0df22a32302fdf58bab63a8
Lukas <lzerucha@gmail.com>
2023-04-09 20:06:21 +0100

    Added splash screen, changed window to borderless, fixed order of Hangman images

--- Hangman.py
+++ Hangman.py
@@ -1,4 +1,6 @@
-# Hangman_GUI main game file
+#!/usr/bin/env python3
+
+# Hangman_GUI main game file with application layout and game logic
 
 from Wordlist import world_list
 import PySimpleGUI as sg
@@ -6,24 +8,56 @@ import random
 
 # Dictionary with all images used in the game
 hangman_img = {
-    0: "img/Hangman_00.png",
-    1: "img/Hangman_01.png",
-    2: "img/Hangman_02.png",
-    3: "img/Hangman_03.png",
-    4: "img/Hangman_04.png",
-    5: "img/Hangman_05.png",
-    6: "img/Hangman_06.png",
-    7: "img/Hangman_07.png",
-    8: "img/Hangman_08.png",
+
+    10: "img/Hangman_10.png",
     9: "img/Hangman_09.png",
-    10: "img/Hangman_10.png"
+    8: "img/Hangman_08.png",
+    7: "img/Hangman_07.png",
+    6: "img/Hangman_06.png",
+    5: "img/Hangman_05.png",
+    4: "img/Hangman_04.png",
+    3: "img/Hangman_03.png",
+    2: "img/Hangman_02.png",
+    1: "img/Hangman_01.png",
+    0: "img/Hangman_00.png",
+
 }
 
 # Install "Young.ttf" (main folder) for best experience, but app will still work without it :)
 font_used = "Young"
 # Create window layout with PySimpleGUI
 sg.theme("black")
-layout = [
+
+# Define the layout for the splash screen
+splash_layout = [
+    [sg.Image("img/splash.png")],
+    [sg.Button("START", font=font_used,
+               border_width=0,
+               key="-START-",
+               button_color="white")]]
+
+# Create the splash screen window
+splash_window = sg.Window('Hangman', splash_layout,
+                          size=(300, 550),
+                          no_titlebar=True,
+                          grab_anywhere=True,
+                          keep_on_top=True,
+                          element_justification='center')
+
+# Show the splash screen and wait for the start button to be pressed
+while True:
+    event, values = splash_window.read()
+    if event == 'START':
+        break
+
+# Close the splash screen window
+splash_window.close()
+
+# Main Game Layout
+game_layout = [
+    [sg.Push(), sg.Image("img/close.png",  # Close button
+                         pad=0, enable_events=True, key="-CLOSE-")],
+    [sg.VPush()],
     [sg.Image(hangman_img[0], key="-HANGMAN-")],
     [sg.Text("", key="-WORD-", font="Young 20")],
     [sg.Text("letters used", font=font_used)],
@@ -42,7 +76,97 @@ layout = [
     [sg.VPush()]
 ]
 
-window = sg.Window("Hangman Game", layout,
+window = sg.Window("Hangman Game", game_layout,
                    size=(300, 550),
                    element_justification="center",
-                   finalize=True)
+                   finalize=True,
+                   no_titlebar=True,
+                   grab_anywhere=True,
+                   keep_on_top=True)
+
+
+def hangman():
+
+    # list with alphabet
+    alphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L',
+                'M', 'N', 'O', 'P', 'R', 'S', 'T', 'U', 'W', 'X', 'Y', 'Z',
+                'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
+                'm', 'n', 'o', 'p', 'r', 's', 't', 'u', 'w', 'x', 'y', 'z']
+    # word choice
+    word = random.choice(world_list).upper()
+    # word letters as list
+    word_letters = [letter for letter in word]
+    # word blanks as list
+    word_blanks = ["_"] * len(word)
+    # number of lives
+    lives = 10
+    # points
+    points = 0
+    # letters already used
+    guessed_letters = ""
+
+    # Main Loop
+    while lives > 0:
+
+        window["-HANGMAN-"].update(hangman_img[lives])
+        window["-WORD-"].update("".join(word_blanks), font="Young 24")
+        window["-USED-LETTERS-"].update(", ".join(guessed_letters))
+        window["-LIVES-"].update(lives)
+
+        event, values = window.read()
+        if event in (sg.WIN_CLOSED, "-CLOSE-"):
+            break
+
+        # accepts only one character, must be from alphabet
+        if len(values["-INPUT-"]) > 1 or values["-INPUT-"] not in alphabet:
+            # delete last char from input
+            window["-INPUT-"].update(values["-INPUT-"][:-1])
+        # if enter is pressed
+        elif event == 'Submit':
+            user_input = window["-INPUT-"].get().upper()
+            window['-INPUT-'].update("")
+
+            # Check if user_input in guessed letters
+            if user_input not in guessed_letters:
+                guessed_letters += user_input
+                window["-USED-LETTERS-"].update(", ".join(guessed_letters))
+
+                # Check if user_input is in word_letters list
+                if user_input in word_letters:
+                    window["-OUT-"].update("Zgadłeś literę. Brawo")
+
+                    # find indexes of correctly guessed letter
+                    for i, letter in enumerate(word_letters):
+                        # and replace blanks with letters
+                        if word_letters[i] == user_input:
+                            word_blanks[i] = word[i]
+                    # output updated word
+                    window["-WORD-"].update("".join(word_blanks), font="Young 24")
+
+                    # win condition
+                    if "".join(word_blanks) == "".join(word_letters):
+                        window["-OUT-"].update("Wygrałeś! Brawo!")
+                        points += 1
+                        window["-POINTS-"].update(str(points))
+                        sg.popup("Wygrana, losuję nowe słowo", font=font_used)
+                        hangman()
+                # lost condition
+                else:
+                    lives = lives - 1
+                    window["-OUT-"].update("Pudło, spróbuj jeszcze raz")
+                    window["-HANGMAN-"].update(hangman_img[lives])
+                    window["-LIVES-"].update(lives)
+                    window["-WORD-"].update("".join(word_blanks), font="Young 24")
+                    if lives == 0:
+                        window["-OUT-"].update("Przegrałeś!")
+                        sg.popup(f"Przegrana. Losuję nowe słowo, słowo to: {word}", font=font_used)
+                        hangman()
+            # output if letter already been chosen
+            else:
+                window["-OUT-"].update("Ta litera już była.")
+
+    window.close()
+
+
+if __name__ == '__main__':
+    hangman()
