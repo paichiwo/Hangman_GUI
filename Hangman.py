8eb44d057bfdb12cf0795b7ff292c4a5e5e7204a
Lukas <lzerucha@gmail.com>
2023-04-09 17:47:56 +0100

    Created application layout with PySimpleGUI

--- Hangman.py
+++ Hangman.py
@@ -19,3 +19,30 @@ hangman_img = {
     10: "img/Hangman_10.png"
 }
 
+# Install "Young.ttf" (main folder) for best experience, but app will still work without it :)
+font_used = "Young"
+# Create window layout with PySimpleGUI
+sg.theme("black")
+layout = [
+    [sg.Image(hangman_img[0], key="-HANGMAN-")],
+    [sg.Text("", key="-WORD-", font="Young 20")],
+    [sg.Text("letters used", font=font_used)],
+    [sg.Text("", key="-USED-LETTERS-", font=font_used)],
+    [sg.Text("lives", font=font_used),
+     sg.Text("10", key="-LIVES-", font="Young 16", text_color="green"),
+     sg.Push(),
+     sg.Text("0", key="-POINTS-", font="Young 16", text_color="green"),
+     sg.Text("points", font=font_used)],
+    [sg.Text("Guess a letter:", font=font_used)],
+    [sg.Input("", size=(10, 1),
+              enable_events=True,
+              key="-INPUT-")],
+    [sg.Button('Submit', visible=False, bind_return_key=True)],
+    [sg.Text("", key="-OUT-", font="Any 10", text_color="yellow")],
+    [sg.VPush()]
+]
+
+window = sg.Window("Hangman Game", layout,
+                   size=(300, 550),
+                   element_justification="center",
+                   finalize=True)
