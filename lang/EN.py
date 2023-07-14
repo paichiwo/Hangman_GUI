33622724d0479255c823450bbc6c807fee1ceefa
paichiwo <lzerucha@gmail.com>
2023-07-14 21:13:14 +0100

    Add EN.py with English localization.

---
+++ lang/EN.py
@@ -0,0 +1,21 @@
+
+EN = {
+    'splash.window.version': 'Version:',
+    'splash.window.link.visit': 'Click to visit my GitHub',
+
+    'settings.window.title': "SETTINGS",
+    'settings.window.select.language': 'Select language:',
+    'settings.window.save.button': 'Save',
+
+    'game.window.used.letters': 'USED LETTERS:',
+    'game.window.lives': 'LIVES',
+    'game.window.points': 'POINTS',
+    'game.window.guess.letter': 'GUESS A LETTER:',
+    'game.window.submit.button': 'Submit',
+    'game.window.good.guess.msg': 'Good guess, letter found',
+    'game.window.wrong.guess.msg': 'Wrong, try again!',
+    'game.window.letter.used.msg': 'Letter used already',
+
+    'popup.window.won.msg': f'YOU WON! \nFind the meaning of: ?\n',
+    'popup.window.meaning.msg': f'YOU LOST! \nDo you want to find the meaning\nof the word: ?\n'
+}
