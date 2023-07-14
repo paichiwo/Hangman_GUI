4f17eb373cdd64d7244f30a2f2df43a6baabf784
paichiwo <lzerucha@gmail.com>
2023-07-14 23:54:49 +0100

    Change EN.py to localization.py - now contains all language packs.

---
+++ lang/localization.py
@@ -0,0 +1,44 @@
+localization = {
+
+    'EN': {
+        'splash.window.version': 'Version',
+        'splash.window.start_button': 'START',
+        'splash.window.github_link': 'Click to visit my GitHub',
+
+        'settings.window.title': 'SETTINGS',
+        'settings.window.select_language': 'Select Language:',
+        'settings.window.save_button': 'Save',
+
+        'game.window.used_letters': 'USED LETTERS:',
+        'game.window.lives': 'LIVES',
+        'game.window.points': 'POINTS',
+        'game.window.guess_letter': 'GUESS A LETTER:',
+        'game.window.submit_button': 'Submit',
+        'game.window.output_msg_good_guess': 'Good guess, letter found',
+        'game.window.output_msg_wrong_guess': 'Wrong, try again!',
+        'game.window.you_won_message': 'YOU WON! \nFind the meaning of: {} ?\n',
+        'game.window.you_lost_message': 'YOU LOST! \nFind the meaning of: {} ?\n',
+        'game.window.letter_used_message': 'Letter used already!'
+    },
+
+    'PL': {
+        'splash.window.version': 'Wersja',
+        'splash.window.start_button': 'START',
+        'splash.window.github_link': 'Kliknij, aby odwiedzić mój GitHub',
+
+        'settings.window.title': 'USTAWIENIA',
+        'settings.window.select_language': 'Wybierz język:',
+        'settings.window.save_button': 'Zapisz',
+
+        'game.window.used_letters': 'UŻYTE LITERY:',
+        'game.window.lives': 'ŻYCIA',
+        'game.window.points': 'PUNKTY',
+        'game.window.guess_letter': 'ZGADNIJ LITERĘ:',
+        'game.window.submit_button': 'Zgaduj',
+        'game.window.output_msg_good_guess': 'Dobrze, litera znaleziona',
+        'game.window.output_msg_wrong_guess': 'Źle, spróbuj ponownie!',
+        'game.window.you_won_message': 'WYGRAŁEŚ! \nZnajdź znaczenie: {} ?\n',
+        'game.window.you_lost_message': 'PRZEGRAŁEŚ! \nZnajdź znaczenie: {} ?\n',
+        'game.window.letter_used_message': 'Litera wykorzystana!'
+    }
+}
